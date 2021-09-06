4353515711b76b2356244d1a9e62b6844bb975b4
Seth Berry <seth.berry@nd.edu>
2021-09-06 12:07:17 -0400

    chapter updates

--- book_render.R
+++ book_render.R
@@ -9,3 +9,9 @@ bookdown::render_book("chapters/",
                       bs4_book(theme = bs_theme(bootswatch = "cyborg")))
 
 bookdown::render_book("chapters/", "bookdown::pdf_book")
+
+
+bookdown::render_book("chapters_ug/", 
+                      bs4_book(theme = bs_theme(bootswatch = "cyborg")))
+
+bookdown::render_book("chapters_ug/", "bookdown::pdf_book")
